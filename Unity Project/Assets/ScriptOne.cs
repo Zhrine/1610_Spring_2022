af1f715098b1bd09118e74d0628847f1e9c395c1
Foster Boone <10787343@uvu.edu>
2022-02-08 13:03:28 -0700

    Added Vars and function

--- Unity Project/Assets/ScriptOne.cs
+++ Unity Project/Assets/ScriptOne.cs
@@ -2,21 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
-{
-    void Update()
+public class VariablesAndFunctions : MonoBehaviour
+{   
+    int myInt = 5;
+    
+    
+    void Start ()
     {
-        if (input.getkeydown(keycode.R))
-        {
-            GetComponent<Renderer>().material.color = Color.Red;
-        }
-        if (input.getkydown(keycode.g))
-        {
-            GetComponent<Renderer>().material.color = Color.Green;
-        }
-        if (input.getkeydown(keycode.B))
-        {
-            GetComponent<Renderer>().material.color = Color.Blue;
-        }
+        myInt = MultiplyByTwo(myInt);
+        Debug.Log (myInt);
+    }
+    
+    
+    int MultiplyByTwo (int number)
+    {
+        int result;
+        result = number * 2;
+        return result;
     }
 }
