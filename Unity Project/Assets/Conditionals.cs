7bf487eac2d01096f6ad2da37ff547d0e4d991c4
Foster Boone <10787343@uvu.edu>
2022-02-24 18:02:06 -0700

    Conditional/Switches Entered

---
+++ Unity Project/Assets/Conditionals.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conditionals : MonoBehaviour
+{
+ if (x <= y)
+ {
+     Console.WriteLine("Dont Continue Further");
+     // block of code to be executed if the condition is True
+ }
+ if (20 > 18) 
+ {
+     Console.WriteLine("20 is greater than 18");
+ }
+}
