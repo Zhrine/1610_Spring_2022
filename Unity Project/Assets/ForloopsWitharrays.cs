0c73ae8935a162ed1ef992d235de43284b458e35
Foster Boone <10787343@uvu.edu>
2022-03-03 19:50:00 -0700

    For Each loops and Forloops Actual commit

---
+++ Unity Project/Assets/ForloopsWitharrays.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine(i);
+}
+
+}
+
--- Unity Project/Assets/Switches.cs
+++ Unity Project/Assets/Switches.cs
@@ -4,43 +4,31 @@ using UnityEngine;
 
 public class Switches : MonoBehaviour
 {
-// switch(expression) 
-{
-  //  case x:
-        // code block
-      //  break;
-   // case y:
-        // code block
-       // break;
- //   default:
-        // code block
-       // break;
-}
-// example
-int day = 4;
-switch (day) 
-{
-  case 1:
-    Console.WriteLine("Monday");
-    break;
-  case 2:
-    Console.WriteLine("Tuesday");
-    break;
-  case 3:
-    Console.WriteLine("Wednesday");
-    break;
-  case 4:
-    Console.WriteLine("Thursday");
-    break;
-  case 5:
-    Console.WriteLine("Friday");
-    break;
-  case 6:
-    Console.WriteLine("Saturday");
-    break;
-  case 7:
-    Console.WriteLine("Sunday");
-    break;
-}
-// Outputs "Thursday" (day 4)
+    //int day = 4;
+//switch (day)
+    {
+        //case 1:
+        //Console.WriteLine("Monday");
+        //break;
+        //case 2:
+        //Console.WriteLine("Tuesday");
+        //break;
+        //case 3:
+        //Console.WriteLine("Wednesday");
+        //break;
+        //case 4:
+        //Console.WriteLine("Thursday");
+        //break;
+        //case 5:
+        //Console.WriteLine("Friday");
+        //break;
+        //case 6:
+        //Console.WriteLine("Saturday");
+        //break;
+        //case 7:
+        //Console.WriteLine("Sunday");
+        //break;
+    }
+   
+    }
 }
